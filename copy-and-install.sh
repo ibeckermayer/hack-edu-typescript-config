1f3937451ee5f9fc2f46405f948a13538db00dde
Isaiah Becker-Mayer <isaiah@goteleport.com>
2022-06-16 20:56:02 -0400

    adding copy-and-install script for personal use

---
+++ copy-and-install.sh
@@ -0,0 +1,53 @@
+cp ~/hack-edu/typescript-config/bac/* ~/hack-edu/typescript-config/bac/.* ~/hack-edu/content/content/courses/was1/bac/; cd ~/hack-edu/content/content/courses/was1/bac; yarn install
+
+
+cp ~/hack-edu/typescript-config/bas/* ~/hack-edu/typescript-config/bas/.* ~/hack-edu/content/content/courses/was1/bas/; cd ~/hack-edu/content/content/courses/was1/bas; yarn install
+
+
+cp ~/hack-edu/typescript-config/cmdi/* ~/hack-edu/typescript-config/cmdi/.* ~/hack-edu/content/content/courses/was1/cmdi/; cd ~/hack-edu/content/content/courses/was1/cmdi; yarn install
+
+
+cp ~/hack-edu/typescript-config/idesi/* ~/hack-edu/typescript-config/idesi/.* ~/hack-edu/content/content/courses/was1/idesi/; cd ~/hack-edu/content/content/courses/was1/idesi; yarn install
+
+
+cp ~/hack-edu/typescript-config/sqli_1/* ~/hack-edu/typescript-config/sqli_1/.* ~/hack-edu/content/content/courses/was1/sqli_1/; cd ~/hack-edu/content/content/courses/was1/sqli_1; yarn install
+
+
+cp ~/hack-edu/typescript-config/sqli_2/* ~/hack-edu/typescript-config/sqli_2/.* ~/hack-edu/content/content/courses/was1/sqli_2/; cd ~/hack-edu/content/content/courses/was1/sqli_2; yarn install
+
+
+cp ~/hack-edu/typescript-config/sqli_3/* ~/hack-edu/typescript-config/sqli_3/.* ~/hack-edu/content/content/courses/was_other/sqli_3/; cd ~/hack-edu/content/content/courses/was_other/sqli_3; yarn install
+
+
+cp ~/hack-edu/typescript-config/ssrf/* ~/hack-edu/typescript-config/ssrf/.* ~/hack-edu/content/content/courses/was1/ssrf/; cd ~/hack-edu/content/content/courses/was1/ssrf; yarn install
+
+
+cp ~/hack-edu/typescript-config/xss_reflected/* ~/hack-edu/typescript-config/xss_reflected/.* ~/hack-edu/content/content/courses/was1/xss_reflected/; cd ~/hack-edu/content/content/courses/was1/xss_reflected; yarn install
+
+
+cp ~/hack-edu/typescript-config/xss_stored/* ~/hack-edu/typescript-config/xss_stored/.* ~/hack-edu/content/content/courses/was1/xss_stored/; cd ~/hack-edu/content/content/courses/was1/xss_stored; yarn install
+
+
+cp ~/hack-edu/typescript-config/xss_dom/* ~/hack-edu/typescript-config/xss_dom/.* ~/hack-edu/content/content/courses/was1/xss_dom/; cd ~/hack-edu/content/content/courses/was1/xss_dom; yarn install
+
+
+cp ~/hack-edu/typescript-config/xxe/* ~/hack-edu/typescript-config/xxe/.* ~/hack-edu/content/content/courses/was1/xxe/; cd ~/hack-edu/content/content/courses/was1/xxe; yarn install
+
+
+cp ~/hack-edu/typescript-config/bfla/* ~/hack-edu/typescript-config/bfla/.* ~/hack-edu/content/content/courses/was1_api/bfla/; cd ~/hack-edu/content/content/courses/was1_api/bfla; yarn install
+
+
+cp ~/hack-edu/typescript-config/bola/* ~/hack-edu/typescript-config/bola/.* ~/hack-edu/content/content/courses/was1_api/bola/; cd ~/hack-edu/content/content/courses/was1_api/bola; yarn install
+
+
+cp ~/hack-edu/typescript-config/mas/* ~/hack-edu/typescript-config/mas/.* ~/hack-edu/content/content/courses/was1_api/mas/; cd ~/hack-edu/content/content/courses/was1_api/mas; yarn install
+
+
+cp ~/hack-edu/typescript-config/oauth1/* ~/hack-edu/typescript-config/oauth1/.* ~/hack-edu/content/content/courses/was_other/oauth1/; cd ~/hack-edu/content/content/courses/was_other/oauth1; yarn install
+
+
+cp ~/hack-edu/typescript-config/oauth2/* ~/hack-edu/typescript-config/oauth2/.* ~/hack-edu/content/content/courses/was_other/oauth2/; cd ~/hack-edu/content/content/courses/was_other/oauth2; yarn install
+
+
+cp ~/hack-edu/typescript-config/ede/* ~/hack-edu/typescript-config/ede/.* ~/hack-edu/content/content/courses/was1_api/ede/; cd ~/hack-edu/content/content/courses/was1_api/ede; yarn install
+
